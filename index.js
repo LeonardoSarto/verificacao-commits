94b8c715396fee54c1da78245c99297a285d75f1
Leonardo <leonardosarto1@hotmail.com>
2023-03-21 22:42:21 -0300

    Exibição em tabela

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ form.addEventListener('submit', function(event){
     if(repositorio == "" || dataInicial == "" || dataFinal == "") {
         alert("Pesquise por um repositório no campo repositório")
     }
-    buscarCommits(repositorio, dataInicial, dataFinal)    
+    buscarCommits(repositorio, dataInicial, dataFinal)
+    exibirAutorNomeRepositorio(repositorio)
 })
 
 function buscarCommits(repositorio, dataInicial, dataFinal) {
+    const token = "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"
+    const options = {
+        headers: {
+            Authorization: `Token ${token}`
+        }
+    }
     const url = `https://api.github.com/repos/${repositorio}/commits?since=${dataInicial}&until=${dataFinal}`
-    fetch(url).then(response => response.json()).then(commits => {
+    fetch(url, options).then(response => response.json()).then(commits => {
         contarCommits(commits)
     })
 }
@@ -33,15 +40,27 @@ function contarCommits(commits) {
         return {data:dataCommit, quantidade:commitsPorDia[dataCommit].quantidade}
     })
 
-    console.log(commitsPorDiaArray)
-    mostrarTela(commitsPorDiaArray)
+    displayCommits(commitsPorDiaArray)
 }
 
-function mostrarTela(commits) {
-    const dados = document.querySelector("#dados")
-    commits.forEach(element => {
-        const h1 = document.createElement("h1")
-        h1.innerHTML = element.data + " - " + element.quantidade
-        dados.appendChild(h1)
-    })
+function exibirAutorNomeRepositorio(repositorio) {
+    const autor = document.querySelector("#autor")
+    const nomeRepositorio = document.querySelector("#nome_repositorio")
+    let aux = repositorio.substring(repositorio.indexOf("m") + 2)
+    autor.value = aux.substring(0, aux.indexOf("/"))
+    nomeRepositorio.value = aux.substring(aux.indexOf("/") + 1)
+    document.querySelector("#autor_repositorio").style.display = "grid"
+}
+
+function displayCommits(commits) {
+    let table = '<table>';
+    table += `<tr><th>Data</th><th>Quantidade</th></tr>`;
+    commits.forEach((commit) => {
+        table = table + `<tr>`;
+        table = table + `<td>${commit.data}</td>`;
+        table = table + `<td>${commit.quantidade}</td>`;
+        table += `</tr>`;
+    });
+    table += "</table>";
+    document.getElementById("commits-table").innerHTML = table;
 }
